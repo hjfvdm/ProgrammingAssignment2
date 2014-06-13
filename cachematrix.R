8c272fcfcff7da8b9a8c580a4b34f516f3cce9de
Rikus van der Merwe <rikus.vandermerwe@underwriteme.co.uk>
2014-06-13 16:11:15 +0100

    Programming Assignment 2: Lexical Scoping. Adding usage example to comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
-## Creates a special matrix where the inverse is cached.
+## Methods for creating a cachable matrix where the inverse is cached and retrieved
 ## The cached inverse is returned if no changes have been made to the matrix.
+##
+## Example usage:
+##
+## invertibleMatrix = rbind(c(1, -1/4), c(-1/4, 1))  
+## cachedMatrix <- makeCacheMatrix(invertibleMatrix)
+## solvedMatrix <- cacheSolve(cachedMatrix)
+## running the solveMatrix again will retrieve the cached result
+## You will see this message: 'getting cached data'
 
 ## Creates a matrix with getters and setters for the inverse 
 
