cecaed042aa154928849c2525e0903e504b08753
Rikus van der Merwe <rikus.vandermerwe@underwriteme.co.uk>
2014-06-13 16:00:57 +0100

    Programming Assignment 2: Lexical Scoping. Adding matrix caching for the solve (inverse) function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special matrix where the inverse is cached.
+## The cached inverse is returned if no changes have been made to the matrix.
 
-## Write a short comment describing this function
+## Creates a matrix with getters and setters for the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+        
+        
 }
 
 
-## Write a short comment describing this function
+## Method will return the cached version of the matrix if it exists,
+## otherwise it will be computed and cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
